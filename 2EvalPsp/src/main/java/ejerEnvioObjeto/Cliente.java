dbe0129bdf094acaa7daf2f9af1a0701a7139c49
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-11-21 21:13:09 +0100

    ejercicio mover objetos hecho

--- 2EvalPsp/src/main/java/ejerEnvioObjeto/Cliente.java
+++ 2EvalPsp/src/main/java/ejerEnvioObjeto/Cliente.java
@@ -2,7 +2,9 @@ package ejerEnvioObjeto;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Cliente {
 
@@ -10,39 +12,37 @@ public class Cliente {
 	static final int PUERTO = 5000;
 	private Persona p;
 	private Socket skCliente;
+	private boolean objModificado = false;
+	private ObjectOutputStream os;
 
-	public Cliente() {
+	public Cliente() throws ClassNotFoundException, UnknownHostException, IOException {
+
 		try {
 			skCliente = new Socket(HOST, PUERTO);
 
 			ObjectInputStream ois = new ObjectInputStream(skCliente.getInputStream());
+			p = (Persona) ois.readObject();// guardar el objeto recibido
+			System.out.println(p.toString());
+			// hasta aki funciona, recibe y lo pinta
 
-			try {
-				p = (Persona) ois.readObject();// guardar el objeto recibido
-				p.setNombre("Carmen");
-				p.setFechaNacimiento("1983, 1, 1");
-				System.out.println(p.toString());
+			p.setNombre("Carmen");
+			p.setFechaNacimiento("1983, 1, 1");
 
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
+			skCliente.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		// enviar persona modificado
-//		ObjectOutputStream os;
-//		try {
-//			os = new ObjectOutputStream(skCliente.getOutputStream());
-//			os.writeObject(p);
-//			os.close();
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		// para enviarlo hay que hacer otro socket
+		Socket xaEnvio = new Socket(HOST, 5100);
+		os = new ObjectOutputStream(xaEnvio.getOutputStream());
+		os.writeObject(p);
+		System.out.println("ahora lo envio");
+		xaEnvio.close();
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, UnknownHostException, IOException {
 		new Cliente();
 
 	}
--- 2EvalPsp/src/main/java/ejerEnvioObjeto/Servidor.java
+++ 2EvalPsp/src/main/java/ejerEnvioObjeto/Servidor.java
@@ -1,51 +1,55 @@
 package ejerEnvioObjeto;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
 	static final int PUERTO = 5000;
-	Persona p = new Persona();
+	private Persona p = new Persona();
+	private ObjectInputStream ois;
+	private Persona pRecibida = new Persona();
 
-	public Servidor() {
+	public Servidor() throws ClassNotFoundException, IOException {
 		ServerSocket skServidor;
-		Socket socketKliente;
+		Socket socket;
 		p.setNif("12345678U");
 		p.setNombre("Maria");
 		p.setApellido("Juana Zubiri");
 		p.setGenero('H');
 		p.setFechaNacimiento("1990, 12, 12");
+		// System.out.println(p.toString());
 		try {
 			skServidor = new ServerSocket(PUERTO);
 			System.out.println("Escutxando el puerto " + PUERTO);
 
-			while ((socketKliente = skServidor.accept()) != null) {
-				System.out.println("Conexion desde: " + socketKliente.getInetAddress());
+			while ((socket = skServidor.accept()) != null) {
+				System.out.println("Conexion desde: " + socket.getInetAddress());
 				// envio del objeto
-				ObjectOutputStream os = new ObjectOutputStream(socketKliente.getOutputStream());
-
+				ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
 				os.writeObject(p);
-				os.close();
-				// recibir el objeto modificado
 
+				// recibir el objeto modificado
+				ServerSocket xaRecibir = new ServerSocket(5100);
+				Socket skrecibirCliente;
+				while ((skrecibirCliente = xaRecibir.accept()) != null) {
+					ois = new ObjectInputStream(skrecibirCliente.getInputStream());
+					pRecibida = (Persona) ois.readObject();
+					System.out.println(pRecibida.toString());
+					skrecibirCliente.close();
+				}
+				socket.close();
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		ObjectInputStream ois = new ObjectInputStream(socketKliente.getInputStream());
-//		try {
-//			p = (Persona) ois.readObject();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Bloque catch generado automáticamente
-//			e.printStackTrace();
-//		} // guardar el objeto recibido
-//		System.out.println(p.toString());
+
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		new Servidor();
 
 	}
