ad75e377c056656e918db47094d9f2fdb2bb2c08
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-11-24 10:56:22 +0100

    martes 24-11-2020 klase

---
+++ 2EvalPsp/src/main/java/Ejercicio_Foto/Cliente.java
@@ -0,0 +1,53 @@
+package Ejercicio_Foto;
+
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Cliente extends JFrame {
+	private Socket skCliente;
+	static final String HOST = "localhost";
+	static final int PUERTO = 5000;
+	private JLabel label;
+
+	public Cliente() throws ClassNotFoundException {
+		setSize(550, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+
+		// label = new JLabel("");
+
+		try {
+			skCliente = new Socket(HOST, PUERTO);
+
+			DataInputStream entrada = new DataInputStream(skCliente.getInputStream());
+			int tamanyo = entrada.readInt(); // guarda el tamaño del fichero que recibira
+			byte recibido[] = new byte[tamanyo]; // array donde guardare los bytes del fichero
+			FileOutputStream f = new FileOutputStream(".//copia.jpg");// donde guardara el fichero recibido y de paso le
+																		// cambio el nombre
+			entrada.read(recibido);// guarda en recibido los bytes enviados por el servidor
+			f.write(recibido);// convierte los bytes en la nueva foto
+
+			// pegar la foto a la label
+			// label = new JLabel(copia);
+
+			entrada.close();
+			skCliente.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		label.setBounds(80, 46, 281, 159);
+		getContentPane().add(label);
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Cliente cli = new Cliente();
+		cli.setVisible(true);
+	}
+}
---
+++ 2EvalPsp/src/main/java/Ejercicio_Foto/Servidor.java
@@ -0,0 +1,54 @@
+package Ejercicio_Foto;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+
+	static final int PUERTO = 5000;
+
+	ServerSocket skServidor;
+	Socket socket;
+	String urlFoto = ".//cnt.jpg";
+	// ImageIcon fotoE = new ImageIcon(urlFoto);
+
+//ver pdf 3.2 es lo del tamaño del fichero
+	public Servidor() {
+		try {
+			skServidor = new ServerSocket(PUERTO);
+			System.out.println("Escutxando el puerto " + PUERTO);
+
+			while ((socket = skServidor.accept()) != null) {
+				System.out.println("Conexion desde: " + socket.getInetAddress());
+				// envio del objeto
+				DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+//				salida.writeObject(fotoE);
+//				salida.close();
+
+				File fich = new File(urlFoto);
+				int tam = (int) fich.length();// tamaño del fichero
+				salida.writeInt(tam);// envio del tamaño del fichero para que lo vaya reservando
+				InputStream is = new FileInputStream(fich);// guardar en el input el fichero para guardarlo en el array
+															// de bytes
+				byte leido[] = new byte[tam]; // array de bytes del tamaño del fichero, aun esta vacio
+				is.read(leido);// guardar en leido los bytes del fichero
+
+				salida.write(leido); // envio del array de bytes
+				is.close();
+				socket.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Servidor();
+	}
+}
